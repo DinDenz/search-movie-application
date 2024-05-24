b5ae3017df7537dfc87ec76bb9fa5c00bcaeebd6
DinDenz <DinDenz@users.noreply.github.com>
2024-05-24 15:48:09 +0300

    add sidebar

---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import { Group, Code } from '@mantine/core';
+import classes from './SideBar.module.css';
+import Logo from '../Logo/Logo';
+
+const data = [
+  { link: '', label: 'Movies' },
+  { link: '', label: 'Rated movies' },
+];
+
+export default function NavbarSimple() {
+  const [active, setActive] = useState('Billing');
+
+  const links = data.map((item) => (
+    <a
+      className={classes.link}
+      data-active={item.label === active || undefined}
+      href={item.link}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(item.label);
+      }}
+    >
+      <span>{item.label}</span>
+    </a>
+  ));
+
+  return (
+    <nav className={classes.navbar}>
+      <div className={classes.navbarMain}>
+        <Group className={classes.header} justify="space-between">
+          <Logo />
+          <Code fw={700}>v3.1.2</Code>
+        </Group>
+        {links}
+      </div>
+
+      <div className={classes.footer}>
+        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+          <span>Change account</span>
+        </a>
+
+        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+          <span>Logout</span>
+        </a>
+      </div>
+    </nav>
+  );
+}
