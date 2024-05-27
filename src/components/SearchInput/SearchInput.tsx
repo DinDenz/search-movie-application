c7af6f943e11702971796c9b59f982750496a9ad
DinDenz <DinDenz@users.noreply.github.com>
2024-05-27 15:23:49 +0300

    begin button

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,5 +1,5 @@
-import { TextInput, TextInputProps, ActionIcon, useMantineTheme, rem } from '@mantine/core';
-import { IconSearch, IconArrowRight } from '@tabler/icons-react';
+import { TextInput, TextInputProps, useMantineTheme, rem, Button, Group } from '@mantine/core';
+import { IconSearch } from '@tabler/icons-react';
 import classes from './SearchInput.module.css';
 
 export default function InputWithButton(props: TextInputProps) {
@@ -12,9 +12,9 @@ export default function InputWithButton(props: TextInputProps) {
       rightSectionWidth={42}
       leftSection={<IconSearch style={{ width: rem(18), height: rem(18) }} stroke={1.5} />}
       rightSection={
-        <ActionIcon size={32} radius="xl" color={theme.primaryColor} variant="filled">
-          <IconArrowRight style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-        </ActionIcon>
+        <Group mt="xs">
+          <Button color={theme.primaryColor} style={{ flex: 1 }}>Show details</Button>
+        </Group>
       }
       {...props}
       className={classes.search_input}
